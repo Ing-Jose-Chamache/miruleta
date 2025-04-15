b628c9e2e18d8d1de265ec1d01e91c146ffedd75
Ing-Jose-Chamache <jchamache@senati.pe>
2025-04-14 23:44:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-# Editado por José Yván Chamache para evitar eliminación automática de Codespace (06/04/2025)
+# Editado por José Yván Chamache para evitar eliminación automática de Codespace (14/04/2025)
